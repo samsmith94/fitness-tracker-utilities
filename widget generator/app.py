42ed1794295ae7623978a45e19d035d72d744279
Sámuel Kovács <sunikovacs@gmail.com>
2020-10-29 22:28:13 +0100

    add better menu system

--- widget generator/app.py
+++ widget generator/app.py
@@ -5,13 +5,27 @@ import tkinter.messagebox
 tk = Tk()
 tk.title("Widget designer")
 
+
+from tkinter.simpledialog import askinteger
+
+
+
+
+frame1=Frame(tk)
+frame1.pack(side=TOP,fill=X)
+
+frame2=Frame(tk)
+frame2.pack(side=TOP, fill=X)
+
+
+
 blank_image = PhotoImage()
 
-ROWS = 15
+ROWS = 31
 
 COLS = ROWS
 
-square_size = 10
+square_size = 7
 
 global_color = "#00FF80"
 array_size = 0
@@ -21,9 +35,78 @@ default_color = "#FFFFFF"
 from tkinter.colorchooser import *
 
 
+def draw_pixel(x, y, color):
+    global buttons
+    
+    global print_tuple
+    
+    global array_size
+    
+    array_size+=1
+    
+    nth_button = x*ROWS + y
+    print(nth_button)
+    
+    
+    buttons[nth_button].config(bg=color[1])
+    
+    print_tuple = int(nth_button/ROWS), nth_button%ROWS, int(color[1][1:3], 16), int(color[1][3:5], 16), int(color[1][5:7], 16)
+    print(print_tuple);
+    pixel_array.append(print_tuple)
+    
+    
 
-select_color = Button(tk, text='Pixel Color')
-select_color.grid(row=ROWS+1, column=COLS+1)
+
+def draw_circle(x0, y0, radius, color):
+	x = radius;
+	y = 0;
+	err = 0;
+
+	while (x >= y):
+		draw_pixel(x0 + x, y0 + y, color)
+		draw_pixel(x0 + y, y0 + x, color)
+		draw_pixel(x0 - y, y0 + x, color)
+		draw_pixel(x0 - x, y0 + y, color)
+		draw_pixel(x0 - x, y0 - y, color)
+		draw_pixel(x0 - y, y0 - x, color)
+		draw_pixel(x0 + y, y0 - x, color)
+		draw_pixel(x0 + x, y0 - y, color)
+
+		if (err <= 0):
+			y += 1
+			err += 2 * y + 1
+
+		if (err > 0):
+			x -= 1
+			err -= 2 * x + 1
+
+
+
+def callback():
+    print("called the callback!")
+
+def square_prompt():
+    prompt = askinteger("Circle", "Input circle center X coordinate:")
+    print(prompt)
+    prompt = askinteger("Circle", "Input circle center Y coordinate:")
+    print(prompt)
+    prompt = askinteger("Circle", "Input circle radius:")
+    print(prompt)
+
+
+fill = PhotoImage(file = r"icons/fill.png").subsample(4)
+rubber = PhotoImage(file = r"icons/rubber.png").subsample(4)
+
+
+b = Button(frame1, compound = LEFT, command=square_prompt, relief=FLAT, image=rubber)
+b.pack(side=LEFT, padx=0, pady=0)
+
+#b = Button(toolbar, relief=FLAT, compound = LEFT, command=callback, image=fill)
+
+#select_color = Button(frame1, text='Pixel Color')
+select_color = Button(frame1, relief=FLAT, compound = LEFT, image=fill)
+#select_color.grid(row=ROWS+1, column=COLS+1)
+select_color.pack(side=LEFT,padx=2)
 
 def getColor():
     global global_color
@@ -66,8 +149,8 @@ def onClick(idx):
 
     
 
-menubar = tkinter.Menu(tk)
-menusize = tkinter.Menu(tk, tearoff=0)
+menubar = tkinter.Menu(frame1)
+menusize = tkinter.Menu(frame1, tearoff=0)
 menusize.add_command(label="small (10x10 with 10 mines)")
 menusize.add_command(label="medium (20x20 with 40 mines)")
 menusize.add_command(label="big (35x35 with 120 mines)")
@@ -80,13 +163,13 @@ tk.config(menu=menubar)
 
 index = 0
 
-for x in range(ROWS):
+for x in range(1, ROWS):
     for y in range(COLS):
 
-        button = Button(tk, image=blank_image, text=" ", font='Times 12', bg='white', fg='white', compound=CENTER, command = lambda idx = index: onClick(idx))
+        button = Button(frame2, image=blank_image, text=" ", font='Times 12', bg='black', fg='black', compound=CENTER, command = lambda idx = index: onClick(idx))
         
         button.config(width=square_size, height=square_size)
-        button.config(bg=default_color)
+        button.config(bg='black')
         button.grid(row=x, column=y, sticky="NWSE") #makes the button expand
         index += 1
         buttons.append(button)
@@ -94,6 +177,7 @@ for x in range(ROWS):
 
 font = font.Font(family = 'Times', size = '8')
 
+"""
 canvas = Canvas(tk, width=160, height=80, bg="blue")
 
 canvas.create_text(40, 20, font = font, text="Hello World", fill="white");
@@ -101,6 +185,18 @@ canvas.create_text(40, 20, font = font, text="Hello World", fill="white");
 canvas.create_oval(81, 0+2, 160, 79+2, fill="orange", outline="orange")
 #canvas.pack()
 canvas.grid(row=ROWS+2, column=COLS+1)
+"""
+
+
+
+szin = askcolor()
+
+#draw_circle(15, 15, 12, szin)
+
+
+draw_circle(15, 15, 12, szin)
+
+draw_circle(15, 15, 8, szin)
 
 
 tk.mainloop()
